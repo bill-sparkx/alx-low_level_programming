62c49b68bca2c5d5bf339701509e0bb2100cc387
bill-sparkx <bernardbill316@gmail.com>
2024-02-07 17:47:50 -0800

    Hash tables

---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * mul -mul two integers
+ * @a: first int 
+ * @b: second int
+ * Return: multiply
+ */
+int mul (int a, int b)
+{
+	return (a * b);
+}
+
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * sub -sub two integers
+ * @a: first int 
+ * @b: second int
+ * Return: subtracted final result
+ */
+int sub (int a, int b)
+{
+	return (a-b);
+}
+
